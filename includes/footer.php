d604afa56af25c9cb308a85e1fef6bbe98d40bc4
Abhay <abhaysingh7295@gmail.com>
2020-07-23 18:20:34 +0530

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <!-- partial -->
         <footer class="footer">
           <div class="container-fluid clearfix">
-            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019 <a href="#">otacodes</a>. All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © <?php echo date('Y')?> Retail Hub. All rights reserved. Design & Developed By <a href="https://bhanushainfosoft.com/" target="_blank">Bhanusha InfoSoft</a></span>
+<!--            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>-->
           </div>
         </footer>
         <!-- partial -->
